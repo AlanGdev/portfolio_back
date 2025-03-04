05f470b9098ed9c076a2687b7d07f7fbfdb9e09e
AlanGdev <alan.grolleau@gmail.com>
2025-03-04 21:45:52 +0100

    Connexion à MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose
+	.connect(
+		'mongodb+srv://alangrolleau:BamsukTqLEc3xhbP@cluster0.psblf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+	)
+	.then(() => console.log('Connexion à MongoDB réussie'))
+	.catch(() => console.log('Connexion à MongoDB échouée'));
+
 app.use((req, res, next) => {
-	res.setHeader('Acces-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization',
