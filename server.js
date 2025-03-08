323f1cf07df3c88f13ea4faf3ec1331a52756e1c
Alan.Grolleau <alan.grolleau@gmail.com>
2025-03-08 17:32:19 +0100

    Corrections pour deploiement

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const errorHandler = (error) => {
 const server = http.createServer(app);
 server.on('error', errorHandler);
 server.on('listening', () => {
-	const address = server.address;
+	const address = server.address();
 	const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
 	console.log('listening on' + bind);
 });
