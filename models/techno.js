f073dfbb543f194f62b38e081f54d0cebb5b1512
Alan.Grolleau <alan.grolleau@gmail.com>
2025-03-11 17:39:18 +0100

    Suppression des entrées prédéfinies dans techno.js(model)

--- models/techno.js
+++ models/techno.js
@@ -5,7 +5,6 @@ const technologySchema = new mongoose.Schema({
 	image: { type: String, required: true }, // URL de l'image
 	domaine: {
 		type: String,
-		enum: ['Front-End', 'Back-End', 'Full-Stack'],
 		required: true,
 	},
 	date_creation: { type: Date, default: Date.now },
